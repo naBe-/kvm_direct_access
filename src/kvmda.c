e500cea6ed376ee13b9144eeeb78150d6f7146b6
Pavel Bliznakov <pavel.bliznakov@strypes.eu>
2015-02-23 15:47:12 +0200

    Implemented basic blocs for creating a virtual machine.

--- src/kvmda.c
+++ src/kvmda.c
@@ -1,14 +1,57 @@
-#include <bool.h>
 #include <fnctl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <linux/kvm.h>
 #include <sys/ioctl.h>
+#include <sys/mman.h>
 #include "kvmda.h"
 
-int create_vm(kvm_t * kvm) {
+void create_vm(kvm_t * kvm) {
     kvm->kvm_fd = open("/dev/kvm", O_RDRW);
     kvm->vm_fd = ioctl(kvm->kvm_fd, KVM_CREATE_VM, 0);
-    
+    ioctl(kvm->vm_fd, KVM_SET_TSS_ADDR, 0xffffffffffffd000);
+    ioctl(kvm->vm_fd, KVM_CREATE_IRQCHIP, 0);
 }
+
+int add_phys_memory(kvm_t * kvm, int mem_size) {
+    void * addr = mmap(NULL, mem_size * MB, PROT_READ | PROT_WRITE,
+        MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+
+    struct kvm_userspace_memory_region region = {
+        .slot = 0,
+        .flags = 0,
+        .guest_phys_addr = 0x100000,
+        .memory_size = mem_size * MB,
+        .userspace_addr = (__u64)addr
+    };
+
+    return ioctl(kvm->vm_fd, KVM_SET_USER_MEMORY_REGION, &region);
+}
+
+int init_vcpu(kvm_t * kvm) {
+    struct kvm_regs regs;
+
+    kvm->vcpu_fd = ioctl(kvm->vm_fd, KVM_CREATE_VCPU, 0);
+    ioctl(kvm->vcpu_fd, KVM_GET_REGS, &regs);
+    regs.rflags = 0x02;
+    regs.rip = 0x100f000;
+    ioctl(kvm->vcpu_fd, KVM_SET_REGS, &regs);
+}
+
+int run_vcpu(kvm_t * kvm) {
+    int res;
+    int kvm_run_size;
+    struct kvm_run * run_state;
+
+    kvm_run_size = ioctl(kvm->kvm_fd, KVM_GET_CPU_MMAP_SIZE, 0);
+    run_state = mmap(NULL, kvm_run_size, PROT_READ | PROT_WRITE,
+            MAP_PRIVATE, kvm->vcpu_fd, 0);
+
+    while(1) {
+        res = ioctl(kvm->vcpu_fd, KVM_RUN, 0);
+
+        
+    }
+}
+
--- src/kvmda.h
+++ src/kvmda.h
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 
 typedef struct kvm {
-    int kvm_fd, sys_fd, vcpu_fd;
-    void * memory;
+    int kvm_fd, vm_fd, vcpu_fd;
+    void * mem_addr;
 } kvm_t;
+
+void create_vm(kvm_t * kvm);
+int add_phys_memory(kvm_t * kvm, int mem_size);
